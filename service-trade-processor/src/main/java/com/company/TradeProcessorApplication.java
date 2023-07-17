ee56b62f6efdaeb4240cb41a4d58fe4376cba7c0
“kodebinder” <kodebinder@gmail.com>
2023-07-17 09:25:24 +0530

    added new module z-test-module

---
+++ service-trade-processor/src/main/java/com/company/TradeProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeProcessorApplication.class, args);
+    }
+
+}
